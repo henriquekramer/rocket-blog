f568482e19c018ce31d17d935ae14fb78772eeec
Henrique <henriquemkramer@gmail.com>
2022-03-30 12:06:15 -0300

    Menu para mobile com JS, footer e responsividade

---
+++ main.js
@@ -0,0 +1,13 @@
+/* Abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', () => nav.classList.toggle('show'));
+}
+
+/*Quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links){
+  link.addEventListener('click', () => nav.classList.remove('show'))
+}
